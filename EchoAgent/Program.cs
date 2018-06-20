73a704b2c02647aa5d463bb3823b0ccb41447c82
Ace <258613763@qq.com>
2018-06-20 23:28:50 +0800

    代码整理

--- EchoAgent/Program.cs
+++ EchoAgent/Program.cs
@@ -15,13 +15,7 @@ namespace HandlerTest
 
             try
             {
-                Console.Write("请选择运行模式：1，服务端；2，客户端  ");
-                var ch = Console.ReadKey().KeyChar;
-                Console.WriteLine();
-                if (ch == '1')
-                    TestServer();
-                else
-                    TestClient();
+                TestServer();
             }
             catch (Exception ex)
             {
@@ -58,40 +52,7 @@ namespace HandlerTest
             // 定时显示性能数据
             _timer = new TimerX(ShowStat, svr, 100, 1000);
         }
-
-        static void TestClient()
-        {
-            var uri = new NetUri("tcp://127.0.0.1:1234");
-            //var uri = new NetUri("tcp://net.newlifex.com:1234");
-            var client = uri.CreateRemote();
-            client.Log = XTrace.Log;
-            client.Received += (s, e) =>
-            {
-                var pk = e.Message as Packet;
-                XTrace.WriteLine("收到：{0}", pk.ToStr());
-            };
-            //client.Add(new LengthFieldCodec { Size = 4 });
-            client.Add<StandardCodec>();
-
-            // 打开原始数据日志
-            var ns = client;
-            ns.LogSend = true;
-            ns.LogReceive = true;
-
-            client.Open();
-
-            // 定时显示性能数据
-            _timer = new TimerX(ShowStat, client, 100, 1000);
-
-            // 循环发送数据
-            for (var i = 0; i < 5; i++)
-            {
-                var str = "你好" + (i + 1);
-                var pk = new Packet(str.GetBytes());
-                client.SendMessageAsync(pk);
-            }
-        }
-
+        
         class User
         {
             public Int32 ID { get; set; }
