f6aa4e0499aadc5c287636bfeef21ea8cb4b0572
Ace <258613763@qq.com>
2018-06-20 17:01:24 +0800

    add init file

---
+++ HandlerTest/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using NewLife.Data;
+using NewLife.Log;
+using NewLife.Net;
+using NewLife.Net.Handlers;
+using NewLife.Threading;
+
+namespace HandlerTest
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            XTrace.UseConsole();
+
+            try
+            {
+                TestClient();
+            }
+            catch (Exception ex)
+            {
+                XTrace.WriteException(ex);
+            }
+
+            Console.WriteLine("OK!");
+            Console.ReadKey();
+        }
+
+        static TimerX _timer;
+        
+        static void TestClient()
+        {
+            //var uri = new NetUri("tcp://127.0.0.1:1234");
+            var uri = new NetUri("tcp://www.armku.com:1234");
+            var client = uri.CreateRemote();
+            client.Log = XTrace.Log;
+            client.Received += (s, e) =>
+            {
+                var pk = e.Message as Packet;
+                XTrace.WriteLine("收到：{0}", pk.ToStr());
+            };
+            //client.Add(new LengthFieldCodec { Size = 4 });
+            client.Add<StandardCodecDemo>();
+
+            // 打开原始数据日志
+            var ns = client;
+            ns.LogSend = true;
+            ns.LogReceive = true;
+
+            client.Open();
+
+            // 定时显示性能数据
+            _timer = new TimerX(ShowStat, client, 100, 1000);
+
+            // 循环发送数据
+            for (var i = 0; i < 1; i++)
+            {
+                var str = "你好" + (i + 1);
+                var pk = new Packet(str.GetBytes());
+                client.SendMessageAsync(pk);
+            }
+        }
+
+        class User
+        {
+            public Int32 ID { get; set; }
+            public String Name { get; set; }
+        }
+
+        static void ShowStat(Object state)
+        {
+            var msg = "";
+            if (state is NetServer ns)
+                msg = ns.GetStat();
+            else if (state is ISocketRemote ss)
+                msg = ss.GetStat();
+
+            Console.Title = msg;
+        }
+    }
+}
---
+++ RpcServerDemo/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using NewLife.Data;
+using NewLife.Log;
+using NewLife.Net;
+using NewLife.Net.Handlers;
+using NewLife.Remoting;
+using NewLife.Threading;
+
+namespace RpcTest
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            XTrace.UseConsole();
+
+            try
+            {                
+                    TestServer();
+            }
+            catch (Exception ex)
+            {
+                XTrace.WriteException(ex);
+            }
+
+            Console.WriteLine("OK!");
+            Console.ReadKey();
+        }
+
+        static TimerX _timer;
+        static ApiServer _server;
+        static void TestServer()
+        {
+            // 实例化RPC服务端，指定端口，同时在Tcp/Udp/IPv4/IPv6上监听
+            var svr = new ApiServer(1234);
+            // 注册服务控制器
+            svr.Register<MyController>();
+            svr.Register<UserController>();
+
+            // 指定编码器
+            svr.Encoder = new JsonEncoder();
+            svr.EncoderLog = XTrace.Log;
+
+            // 打开原始数据日志
+            var ns = svr.Server as NetServer;
+            ns.Log = XTrace.Log;
+            ns.LogSend = true;
+            ns.LogReceive = true;
+
+            svr.Log = XTrace.Log;
+            svr.Start();
+
+            _server = svr;
+
+            // 定时显示性能数据
+            _timer = new TimerX(ShowStat, ns, 100, 1000);
+        }
+
+        static void ShowStat(Object state)
+        {
+            var msg = "";
+            if (state is NetServer ns)
+                msg = ns.GetStat();
+            else if (state is ISocketRemote ss)
+                msg = ss.GetStat();
+
+            Console.Title = msg;
+        }
+    }
+}
